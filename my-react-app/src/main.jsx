ce3175e871ca04af5de29b3d43c4abcb1142f7ba
Ilarce1999 <ilarce@sscgi.com>
2025-04-24 16:00:01 +0800

    End of Server section

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -2,8 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// import customFetch from '../../utils/customFetch.js'
 
-fetch('/api/v1/test').then((res)=>res.json()).then((data)=> console.log(data));
+//const resp = await customFetch.get('/test');
+//console.log(resp);
+
+
+//fetch('/api/v1/test').then((res)=>res.json()).then((data)=> console.log(data));
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
