fcb39635bff6690b57b9958dcf6f607f91ceb4a8
Ilarce1999 <ilarce@sscgi.com>
2025-05-25 16:20:41 +0800

    Fixed authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
-import { comparePassword, hashPassword } from '../../utils/passwordUtils.js';
+import { comparePassword, hashPassword } from '../utils/passwordUtils.js';
 import { UnauthenticatedError } from '../errors/customErrors.js';
 import { createJWT } from '../utils/tokenUtils.js';
 
