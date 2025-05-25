5edb52348270dfead47a5c959cc736b4b45f0241
Ilarce1999 <ilarce@sscgi.com>
2025-05-25 15:41:19 +0800

    Fixed the authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 import { comparePassword, hashPassword } from '../../utils/passwordUtils.js';
-import { UnauthenticatedError } from '../errors/customErrors.js';
+import { UnauthenticatedError } from '../../errors/customErrors.js';
 import { createJWT } from '../../utils/tokenUtils.js';
 
 export const register = async (req, res) => {
