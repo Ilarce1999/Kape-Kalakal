7f9a9ce4d09ee87f0ea7e8a35eb1baa2e19e04bc
Ilarce1999 <ilarce@sscgi.com>
2025-05-29 11:59:16 +0800

    Revert to original

--- my-react-app/src/pages/superadmin/SuperadminComponents/SuperadminLayout.jsx
+++ my-react-app/src/pages/superadmin/SuperadminComponents/SuperadminLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './NavBar';
-import Footer from '../components/Footer';
+import Footer from '../../../components/ooter';
 import { Outlet } from 'react-router-dom';
 
 const SuperAdminLayout = ({ children }) => {
