08ef820b4af7af73f0a7a476325426766908c3f0
Ilarce1999 <ilarce@sscgi.com>
2025-05-29 11:10:38 +0800

    Revert back to original

--- my-react-app/src/pages/admin/AdminComponents/AdminLayout.jsx
+++ my-react-app/src/pages/admin/AdminComponents/AdminLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './NavBar';
-import Footer from '../components/Footer';
+import Footer from '../../../components/footer';
 import { Outlet } from 'react-router-dom';
 
 const AdminLayout = ({ children }) => {
